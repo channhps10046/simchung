2c672d1513d5a88bf28c319a6e82cefcf02225e4
channhps10046 <ngohongchan12a4@gmail.com>
2021-05-02 10:53:46 +0700

    first commits

---
+++ build/precache-manifest.4c0f546adbd7611f2df933ed38a05a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e2d891f8bc997f06686ef8ae431316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "686cf55309981db12e18",
+    "url": "/static/css/main.daa4f962.chunk.css"
+  },
+  {
+    "revision": "829a3f1e2fa867b94d5e",
+    "url": "/static/js/2.036446bc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.036446bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686cf55309981db12e18",
+    "url": "/static/js/main.811f7691.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
